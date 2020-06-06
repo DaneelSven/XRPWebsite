dfd67d758310185719d017b0955a2046a1e3efae
Sven <sven4696@gmail.com>
2020-06-06 15:36:07 +0200

    first upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Test from './Components/test'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Test/>
+      <Test/>
+
+    </div>
+  );
+}
+
+export default App;
